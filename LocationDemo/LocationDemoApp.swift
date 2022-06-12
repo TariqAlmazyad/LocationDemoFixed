2a04c734a08949f2f86ae748f5575d2292891a6f
Tariq Almazyad <Tariq.Almazyad@gmail.com>
2022-06-12 17:23:44 +0300

    Initial Commit

---
+++ LocationDemo/LocationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationDemoApp.swift
+//  LocationDemo
+//
+//  Created by Tariq Almazyad on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LocationView()
+        }
+    }
+}
